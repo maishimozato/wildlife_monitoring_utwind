bfd43083e10da78b60483bead379e32036fc8c5c
Lorraine Wong <lorrainewong@Lowrains-MacBook-Air.local>
2025-11-06 20:07:26 -0500

    2nd commit

--- lorraine.py
+++ lorraine.py
@@ -1,1 +1,13 @@
-git status
+import os
+
+print("hello world")
+
+bats = 5
+
+for bat in range(bats):
+    print("bat detected! Wind turnbine stopping...")
+    print("stop windturbine count:", bat+1)
+    print("bat gone\n")
+    bats -= 1
+
+print("all bats are saved")
