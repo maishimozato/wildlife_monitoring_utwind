c40b21cdc0c1202d798932821c7e3ef335a28990
yuechen <yuechen.hao@mail.utoronto.ca>
2025-11-06 19:53:06 -0500

    Create pythontest_yh.py

---
+++ pythontest_yh.py
@@ -0,0 +1,15 @@
+#hello world
+attendees = ["Lorraine Wong",
+"Stella Cook",
+"Anne-Shirley Chao",
+"Charlie Whitehead",
+"Tasneem Salek",
+"Jenny Yin",
+"Alex Kim",
+"Sebastian Lee"]
+
+
+
+for i in attendees:
+    print("greeting team member " + i)
+    
