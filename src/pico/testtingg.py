389fa5158eb375ce9ea7830034286f2f3a03f20c
Tasneem Salek <tasneemsalek@Tasneems-MacBook-Pro.local>
2026-03-13 17:20:14 -0400

    trial

--- src/pico/testtingg.py
+++ src/pico/testtingg.py
@@ -1,14 +1,14 @@
-import rp2                                #RP2040 PIO
-from machine import Pin
-import time
+import rp2                                  # import RP2040 PIO library!!
+from machine import Pin                     # to control gpio pins on the pico             
+import time                                 # we need time funcitons so...
 
-# pins
-PDM_CLK = 2
-PDM_DATA = 3
+# pins, numbers 2 and 3 represent the pin no. where the mics clock and data are connected
+PDM_CLK = 2                                 # declared as variables for debugging(?) purposes                                
+PDM_DATA = 3                                # + in case we decide to rewire this thing
 
 # set up clock pin input/output 
-clk = Pin(PDM_CLK, Pin.OUT)
-data = Pin(PDM_DATA, Pin.IN)
+clk = Pin(PDM_CLK, Pin.OUT)                 # the mic doesnt make its clock, so the pico has to send clocl to mic 
+data = Pin(PDM_DATA, Pin.IN)                # self-explanatory; pico gets the data from mic as input
 
 # simple PDM clock generator (around 1 MHz)
 @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
