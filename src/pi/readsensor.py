21ff46b4f9be521eaad5d5a26eb4ef20c1833818
yuechen <yuechen.hao@mail.utoronto.ca>
2026-06-27 11:54:02 -0400

    Update readsensor.py

--- src/pi/readsensor.py
+++ src/pi/readsensor.py
@@ -1,12 +1,31 @@
-print("should run \"sudo raspi-config\"" + " and then \"pip3 install pyserial\"")
+#print("should run \"sudo raspi-config\"" + " and then \"pip3 install pyserial\"")
 
 import serial
 
 ser = serial.Serial("/dev/serial0", 115200, timeout=1)
 
+object_dectected = False
 while True:
     if ser.read() == b'\x59' and ser.read() == b'\x59':
         frame = ser.read(7)
         dist = frame[0] + frame[1]*256
         strength = frame[2] + frame[3]*256
         print(f"Distance: {dist} cm | Strength: {strength}")
+        if dist < 500:  # Example threshold for bat detection
+            object_dectected = True
+
+
+"""
+bash:
+stty -F /dev/serial0 115200 raw -echo
+"""
+"""
+
+with open("/dev/serial0", "rb", buffering=0) as ser:
+    while True:
+        if ser.read(1) == b'\x59' and ser.read(1) == b'\x59':
+            frame = ser.read(7)
+            dist = frame[0] + frame[1] * 256
+            strength = frame[2] + frame[3] * 256
+            print(f"Distance: {dist} cm | Strength: {strength}")
+"""
